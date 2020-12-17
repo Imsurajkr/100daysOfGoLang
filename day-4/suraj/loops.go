9d51b36db1f064231f2bafd58f46808ceb5c6320
Suraj Kumar <suraj.kumar@fiftyfivetech.io>
2020-12-17 23:34:17 +0530

    updated day4 Commit

---
+++ day-4/suraj/loops.go
@@ -0,0 +1,17 @@
+package main
+
+// This is leared from the codes singh Go
+import "fmt"
+
+func main() {
+	// Loops
+	// For Loop
+	for i := 1; i <= 10; i++ {
+		for j := 2; j <= 2; j++ {
+			fmt.Println(i * j)
+		}
+		//fmt.Println(i)
+	}
+}
+
+// This program print the tables of 2
