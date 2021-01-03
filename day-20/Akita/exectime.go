0c468d1a64cc6ffbbd02755e447c7c3f115df4f1
Ankita <ankitarao01011998@gmail.com>
2021-01-02 22:53:01 -0400

    time package

---
+++ day-20/Akita/exectime.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	time.Sleep(time.Second)
+	duration := time.Since(start)
+	fmt.Println("It took time.Sleep(1)", duration, "to finish.")
+	start = time.Now()
+	time.Sleep(2 * time.Second)
+	duration = time.Since(start)
+	fmt.Println("It took time.Sleep(2)", duration, "to finish.")
+	start = time.Now()
+	for i := 0; i < 200000000; i++ {
+		_ = i
+	}
+	duration = time.Since(start)
+	fmt.Println("It took the for loop", duration, "to finish.")
+	sum := 0
+	start = time.Now()
+	for i := 0; i < 200000000; i++ {
+		sum += i
+	}
+	duration = time.Since(start)
+	fmt.Println("It took the for loop", duration, "to finish.")
+}
