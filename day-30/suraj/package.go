f2e4ffa4a2877302aa2fc0a9419a7b92f4ce0cf1
suraj kumar <infosurajkumard@gmail.com>
2021-01-12 23:01:06 +0530

    updated day 30 codes

---
+++ day-30/suraj/package.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+ 
+/* global variable declaration */
+var g int = 20
+ 
+func main() {
+   /* local variable declaration */
+   var g int = 10
+ 
+   fmt.Printf ("value of g = %d\n",  g)
+}package main
+
+import "fmt"
+ 
+/* global variable declaration */
+var g int = 20
+ 
+func main() {
+   /* local variable declaration */
+   var g int = 10
+ 
+   fmt.Printf ("value of g = %d\n",  g)
+}
