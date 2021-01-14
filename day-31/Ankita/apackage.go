5945503db323ef1d2a42d2b41b982412b2f1584f
Ankita <ankitarao01011998@gmail.com>
2021-01-13 23:32:32 -0400

    constructing own package

---
+++ day-31/Ankita/apackage.go
@@ -0,0 +1,15 @@
+/*
+ A package contains multiple files and multiple diectories
+*/
+
+package apackage
+
+import "fmt"
+
+func A() {
+	fmt.Println("Hello Calling from A function of apackage")
+}
+
+func B() {
+	fmt.Println("Hello Calling from B function of apackage")
+}
