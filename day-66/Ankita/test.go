a0e1c4f1ed670f8d629cfad5ac0693c09677b9fe
Ankita <ankitarao01011998@gmail.com>
2021-02-17 22:24:40 +0530

    it's a long journey and we have reached day-66

---
+++ day-66/Ankita/test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"reflect"
+	"testing/quick"
+	"time"
+)
+
+func main() {
+	type point3D struct {
+		X, Y, Z int8
+		S       float32
+	}
+	ran := rand.New(rand.NewSource(time.Now().Unix()))
+	myValues := reflect.TypeOf(point3D{})
+	x, _ := quick.Value(myValues, ran)
+	fmt.Println(x)
+}
