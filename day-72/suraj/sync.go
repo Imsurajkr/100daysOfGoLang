6a4e0be337d27d5e6ee9a2325d7afb1fc86c4435
suraj kumar <infosurajkumard@gmail.com>
2021-02-24 23:53:37 +0530

    updated the synchronization code

---
+++ day-72/suraj/sync.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func worker(done chan bool) {
+	fmt.Print("working...")
+	time.Sleep(time.Second)
+	fmt.Println("done")
+
+	done <- true
+}
+
+func main() {
+
+	done := make(chan bool, 1)
+	go worker(done)
+
+	<-done
+}
