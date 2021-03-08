106ebe3e6142b9f8bce738cf8546ac93d3fd2453
Ankita <ankitarao01011998@gmail.com>
2021-03-08 23:00:21 +0530

    race

---
+++ day-85/Ankita/raceC.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"sync"
+)
+
+func main() {
+	arguments := os.Args
+	if len(arguments) != 2 {
+		fmt.Println("Give me a natural number!")
+		return
+	}
+	numGR, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	var waitGroup sync.WaitGroup
+	var i int
+	k := make(map[int]int)
+	k[1] = 12
+	for i = 0; i < numGR; i++ {
+		waitGroup.Add(1)
+		go func() {
+			defer waitGroup.Done()
+			k[i] = i
+		}()
+	}
+	k[2] = 10
+	waitGroup.Wait()
+	fmt.Printf("k = %#v\n", k)
+}
